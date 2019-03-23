35feb62c373f596a7217a17f8f1d1a74c95a5db5
gaohe <1027785756@qq.com>
2019-03-23 11:26:30 +0800

    1

--- 46全排列.py
+++ 46全排列.py
@@ -10,40 +10,55 @@ from collections import deque
 
 
 class Solution():
-    # def __init__(self):
-    #     self.results = []
-    # def test(self, nums):
-    #     self.perm(nums, 0, len(nums)-1)
-    #     return self.results
-    #
-    #
-    # def perm(self, nums, position, end):
-    #     if position == end:
-    #         temp = copy.deepcopy(nums) # 这个地方是关键，在递归过程中nums指向的内存区域被共享，递归返回上层是会影响已经存储的nums值，因此用深拷贝
-    #         self.results.append(temp)
-    #     else:
-    #         for index in range(position, end + 1):
-    #             nums[index], nums[position] = nums[position], nums[index]
-    #             self.perm(nums, position + 1, end)
-    #             nums[index], nums[position] = nums[position], nums[index]
+
+    def __init__(self):
+        self.result = []
+
+    def test(self, nums, start, end):
+        self.perm(nums, start, end)
+        final_result = []
+        for i in range(len(self.result)):
+            final_result.append(self.result[i][start:end+1])
+        return final_result
+
+
+    def perm(self, nums, start, end):
+        """
+
+        :param nums:
+        :param start:
+        :param end:
+        :return:
+        """
+
+        if start == end: # 全排列完成或者只有一个元素
+            _ = []
+            for i in range(0, end+1): # 虽然是对指定部分做全排列，但是还是要全部分返回
+                _.append(nums[i])
+            self.result.append(_)
+
+        for i in range(start, end+1):
+            nums[i], nums[start] = nums[start], nums[i]
+            self.perm(nums, start+1, end)
+            nums[i], nums[start] = nums[start], nums[i]
 
 
     # 用队列实现全排列
     from collections import deque
-    def permut(self, nums):
-        if not nums:
-            return [[]]
-        d = deque([[]])
-        while 1:
-            if len(d[0]) == len(nums): #完成全排列
-                break
-            temp = d.popleft()
-            for val in nums:
-                if val in temp:
-                    continue
-                else:
-                    d.append(temp + [val])
-        return list(d)
+    # def permut(self, nums):
+    #     if not nums:
+    #         return [[]]
+    #     d = deque([[]])
+    #     while 1:
+    #         if len(d[0]) == len(nums): #完成全排列
+    #             break
+    #         temp = d.popleft()
+    #         for val in nums:
+    #             if val in temp:
+    #                 continue
+    #             else:
+    #                 d.append(temp + [val])
+    #     return list(d)
 
 
 
@@ -52,6 +67,7 @@ class Solution():
 
 if __name__ == "__main__":
     s = Solution()
-    nums = ["a", "b", "c"]
-    a = s.permut(nums)
+    nums = ["a", "b", "c", "d"]
+    # a = s.permut(nums)
+    a = s.test(nums, 1, len(nums) - 1)
     print(a)
