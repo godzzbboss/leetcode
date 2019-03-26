118280f1182e302ce3b100a821355c12f89ef297
gaohe <1027785756@qq.com>
2019-03-26 15:32:38 +0800

    1

---
+++ 剑指offer/二进制1的个数.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+__author__ = "BigBrother"
+
+"""
+
+
+class Solution:
+    def NumberOf1(self, n):
+        # write code here
+        count = 0
+        nums = 0 # 移位次数
+        while n and nums < 32:
+            if n & 1 == 1:
+                count += 1
+            n = n >> 1
+            nums += 1
+        return count
