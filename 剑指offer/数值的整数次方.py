71be038a6a3e7081c864f751a7a515b1708ed41d
gaohe <1027785756@qq.com>
2019-03-26 17:27:50 +0800

    1

--- 剑指offer/数值的整数次方.py
+++ 剑指offer/数值的整数次方.py
@@ -7,11 +7,36 @@ __author__ = "BigBrother"
 
 
 class Solution():
+    # def test(self, base, exponent):
+    #     return base ** exponent
+
     def test(self, base, exponent):
-        return base ** exponent
+        """快速幂"""
+        if base == 0 and exponent < 0:
+            return -1
+        if exponent == 0:
+            return 1
+        count = 0
+        result = 1
+        flag = 0
+        if exponent < 0:
+            flag = 1
+        exponent = abs(exponent)
+
+        while exponent and count < 32:
+            if exponent & 1 == 1:
+                result *= base
+            exponent = exponent >> 1
+            base *= base
+            count += 1
+        if flag == 1:
+            result = 1 / result
+        return result
+
+
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.test(4,5))
+    print(s.test(2,-5))
 
