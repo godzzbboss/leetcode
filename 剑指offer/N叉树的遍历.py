c8939abda498f755741474aeff25c9816843010e
gaohe <1027785756@qq.com>
2019-04-02 11:16:30 +0800

    1

---
+++ 剑指offer/N叉树的遍历.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+__author__ = "BigBrother"
+
+"""
+
+class TreeNode:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def pre_travel(self, root):
+        pass
+
+    def in_travel(self, root):
+        pass
+
+    def after_travel(self, root):
+        pass
+
+    def layer_travel(self, root):
+        pass
+
