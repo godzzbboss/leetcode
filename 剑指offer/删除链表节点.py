52608ca953069d8b3e27639e49a76a6f216f83e0
gaohe <1027785756@qq.com>
2019-04-13 17:14:00 +0800

    1

--- 剑指offer/删除链表节点.py
+++ 剑指offer/删除链表节点.py
@@ -32,11 +32,6 @@ class Solution():
         return root
 
 
-
-
-
-
-
 if __name__ == "__main__":
     s = Solution()
     print(s.test())
--- 剑指offer/数组中重复数字.py
+++ 剑指offer/数组中重复数字.py
@@ -11,25 +11,42 @@ class Solution(object):
         """时间复杂度O(n)"""
         if len(array) == 0:
             return -1
-        for i in array:
-            if i < 0 or i > len(array) - 1:
-                return -1
-
         d = {}
         duplicate_num = []
         for i in array:
+            if i < 0 or i > len(array) - 1:
+                return -1
             if d.__contains__(i):
-                d[i] += 1
                 if i not in duplicate_num:
                     duplicate_num.append(i)
             else:
-                d[i] = 0
-                d[i] += 1
+                d[i] = 1
+
         return duplicate_num
+
+    # 空间复杂度为O（1）
+    # def duplicate(self, array, duplication):
+    #     # write code here
+    #     if not array:
+    #         return False
+    #     for i in range(len(array)):
+    #         if array[i] < 0 or array[i] > len(array) - 1:
+    #             return False
+    #     for i in range(len(array)):
+    #         # print(i)
+    #         while i != array[i]:
+    #             if array[i] == array[array[i]]:
+    #                 duplication[0] = array[i]
+    #                 return True
+    #             temp = array[i]
+    #             array[i], array[temp] = array[temp], array[i]
+    #     return False
+
 
 
 if __name__ == "__main__":
     s = Solution()
-    array = [1,2,3,3,4,2]
-    print(s.get_duplicate_num(array))
-
+    array = [1,2,3,0]
+    # print(s.get_duplicate_num(array))
+    duplication = [-1]
+    print(s.duplicate(array, duplication))
