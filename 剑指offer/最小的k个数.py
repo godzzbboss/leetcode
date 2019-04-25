80547e20ab49e626eb593f5fdbe68f406613737e
godzzbboss <1027785756@qq.com>
2019-04-25 16:04:14 +0800

    1

--- 剑指offer/最小的k个数.py
+++ 剑指offer/最小的k个数.py
@@ -7,29 +7,20 @@ __author__ == "BigBrother"
 
 import heapq
 class Solution:
-    # def test(self, array, k):
-    #     """使用最大堆实现"""
-    #     if not array or len(array) < k or k == 0:
-    #         return []
-    #
-    #     heap = [] # 建立一个空堆
-    #     for i in range(k):
-    #         heapq.heappush(heap, -array[i])
-    #
-    #     for j in range(k, len(array)):
-    #         if array[j] < -heap[0]:
-    #             heapq.heapreplace(heap, -array[j])
-    #
-    #     return [-i for i in reversed(sorted(heap))]
-
     def test(self, array, k):
-        """使用partition函数实现, 基本思想是找到排序后第k个数的下标"""
-        pass
+        """使用最大堆实现"""
+        if not array or len(array) < k or k == 0:
+            return []
 
-    def partiton(self, array):
+        heap = [] # 建立一个空堆
+        for i in range(k):
+            heapq.heappush(heap, -array[i]) # heapq中实现的是最小堆，所以传入负数
 
+        for j in range(k, len(array)):
+            if array[j] < -heap[0]:
+                heapq.heapreplace(heap, -array[j])
 
-
+        return [-i for i in reversed(sorted(heap))]
 
 
 if __name__ == "__main__":
