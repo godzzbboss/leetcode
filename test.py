1aeeab96272fc98c8abc1458381192177841203d
godzzbboss <1027785756@qq.com>
2019-05-15 11:24:01 +0800

    1

--- test.py
+++ test.py
@@ -1,14 +1,74 @@
 # -*- coding: utf8 -*-
+"""
+    first problem
+"""
+def solution1(array):
+    """positive num to left, negative num to right"""
+    if not array:
+        return []
+    p1 = 0
+    p2 = len(array)-1
 
-def Add(num1, num2):
-    # write code here
-    """使用异或操作和与操作完成"""
-    while num2:
-        temp = (num1 ^ num2) & 0xFFFFFFFF
-        num2 = ((num1 & num2) << 1) & 0xFFFFFFFF
-        num1 = temp
-    if num1 > 0x7FFFFFFF: # 这里的num1的最高位符号位真实的数，如果num1>0x7fffffff,说明num1最高位为1，应该是负数
-        return ~(num1 ^ 0xFFFFFFFF) # 将无符号数变为有符号数
-    return num1 # 如果 num1为正数，则直接返回
+    while p1 < p2:
+        # suppose zero is positive
+        while p1 < p2 and array[p1] >= 0:
+            p1 += 1
+        while p1 < p2 and array[p2] < 0:
+            p2 -= 1
+        array[p1], array[p2] = array[p2], array[p1]
+        p1 += 1
+        p2 -= 1
+    return array
 
-print(Add(-1,-1))
+def solution2(dict):
+    if not dict:
+        return {}
+    res = {}
+    for k, val in dict.items():
+        k_ = k.split(".")
+        new_item = solution2_helper1(k_, val, 0)
+        new_item_key = list(new_item.keys())[0]
+        # print(res)
+        if new_item_key in res:
+            res[new_item_key] = solution2_helper2(res[new_item_key], new_item[new_item_key])
+        else:
+            res.update(solution2_helper1(k_, val, 0))
+    return res
+
+
+
+def solution2_helper1(keys, val, idx):
+    """keys is a list of char"""
+    res = {}
+    if len(keys) == 1:
+        res[keys[0]] = val
+    else:
+        res[keys[0]] = solution2_helper1(keys[idx + 1: ], val, idx)
+    return res
+
+def solution2_helper2(dict1, dict2):
+    """dict1 or dict2 is a dictionary that only has one elem"""
+    res = {}
+    k1, k2 = list(dict1.keys())[0], list(dict2.keys())[0]
+    val1, val2 = dict1[k1], dict2[k2]
+    if k1 != k2:
+        res[k1] = val1
+        res[k2] = val2
+        return res
+    else:
+        res[k1] = solution2_helper2(val1, val2)
+
+    return res
+
+
+
+if __name__ == "__main__":
+    print("solution of first problem:", end="")
+    array = [6, 4, -3, 5, -2, -1, 0, 1, -9]
+    print(solution1(array))
+
+    print("solution of second problem:", end="")
+    d1 = { "A": 1, "B.A": 2, "B.B": 3, "CC.D.E": 4, "CC.D.F": 5}
+    print(solution2(d1))
+
+
--- 剑指offer/二叉搜索树的第k大节点.py
+++ 剑指offer/二叉搜索树的第k大节点.py
@@ -9,22 +9,22 @@ __author__ == "BigBrother"
 class Solution:
     def test(self, root, k):
         """中序遍历二叉树，第k个节点即为第k大节点"""
+        if not root or k <= 0:
+            return None
         count = 0
-        return self.in_order(root, k, count)
+        return self.in_order(root, k, count)[0]
 
     def in_order(self, root, k, count):
-        if not root or k == 0:
-            return count, None
-
-        count, l = self.in_order(root.left, k, count) # 为什么要return count？？？
-        if l:
-            return count, l
-        count += 1 #
+        if not root or k <= 0:
+            return None
+        target, count = self.in_order(root.left, k, count)
+        if target:
+            return target, count
+        count += 1
         if count == k:
-            return count, root
-
-        count, r = self.in_order(root.right, k, count)
-        return count, r # 注意这个地方，不管r是否为空都返回，返回空说明没找到第k个节点
+            return root, count
+        target, count = self.in_order(root.right, k, count)
+        return target, count
 
 if __name__ == "__main__":
     pass
