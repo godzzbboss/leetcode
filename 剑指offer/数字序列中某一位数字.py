da4b801aa1c23d9fce48aa746585654589648fc0
godzzbboss <1027785756@qq.com>
2019-07-03 10:07:35 +0800

    Update 数字序列中某一位数字.py

--- 剑指offer/数字序列中某一位数字.py
+++ 剑指offer/数字序列中某一位数字.py
@@ -21,8 +21,8 @@ class Solution:
         while True:
             if n <= self.helper1(digits) * digits: # 在digits位数中寻找
                 start_num = 10 ** (digits - 1) # digits位数的开始数字
-                num = start_num + ( n + digits - 1) // digits - 1 # 第n位数在num中，（n+i-1）//i表示 n/i 向上取整
-                for i in range(n % digits):
+                num = start_num + ( n + digits - 1) // digits - 1 #（n+i-1）//i表示 n/i 向上取整, 第n位数在num中
+                for i in range(digits - n % digits): # 比如第n位数字为1234的第2位数，1234->12, 然后对10取余 
                     num //= 10
                 return num % 10
             else:
