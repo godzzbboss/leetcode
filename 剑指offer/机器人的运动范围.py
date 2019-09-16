a18cf57401fc6ee363e59711d016ab747b313d39
godzzbboss <1027785756@qq.com>
2019-09-16 15:03:21 +0800

    1

--- 剑指offer/机器人的运动范围.py
+++ 剑指offer/机器人的运动范围.py
@@ -7,38 +7,38 @@ __author__ = "BigBrother"
 
 
 class Solution():
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     def test(self, threshold, rows, cols):
         flag = [[False] * cols for _ in range(rows)]
-        return self.get_result(threshold, rows, cols, 0, 0, flag)
+        return self.dfs(threshold, rows, cols, 0, 0, flag)
 
-
-    def get_result(self, threshold, rows, cols, i, j, flag):
-        """判断（i，j）是否可以进入"""
+    def dfs(self, threshold, rows, cols, i, j, flag):
+        """返回从[i，j]开始移动所能到达的格子数"""
         if i < 0 or j < 0 or i >= rows or j >= cols:
             return 0
-        if self.transform(i) + self.transform(j) > threshold:
+        if self.transform(i) + self.transform(j) > threshold or flag[i][j] == True:
             return 0
-
         # 可以进入
-        if self.transform(i) + self.transform(j) <= threshold and flag[i][j] == False:
-            flag[i][j] = True
-            # 判断其周围的元素
-            return 1 + self.get_result(threshold, rows, cols, i-1, j , flag) + self.get_result(threshold, rows, cols, i+1, j , flag) + self.get_result(
-                threshold, rows, cols, i, j-1, flag) + self.get_result(threshold, rows, cols, i, j+1 , flag)
-        return 0
+        flag[i][j] = True
+        # 判断其周围的元素
+        res = 0
+        for k in range(4):
+            x_ = i + self.dx[k]
+            y_ = j + self.dy[k]
+            res += self.dfs(threshold, rows, cols, x_, y_ , flag)
+        return 1 + res
 
     def transform(self, num):
         """将一个数转为各个位相乘"""
         sum = 0
         while num:
             sum += num % 10 # 取个位数
-            num = num //10
+            num //= 10
 
         return sum
 
-
-
 if __name__ == "__main__":
     s = Solution()
-    print(s.test())
+    print(s.test(7,4,5))
 
