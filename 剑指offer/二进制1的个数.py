12400771efd3b5728e9ed99429f7032600a1e553
godzzbboss <1027785756@qq.com>
2019-09-21 23:00:34 +0800

    1

--- 剑指offer/二进制1的个数.py
+++ 剑指offer/二进制1的个数.py
@@ -9,14 +9,11 @@ __author__ = "BigBrother"
 class Solution:
     def NumberOf1(self, n):
         # write code here
+        n = n & 0xffffffff # 将n转为无符号整数
         count = 0
-        nums = 0 # 移位次数
-        while n and nums < 32: # 在python中，整数的位数并不是32位，所以自己限定一下
+        while n: # 在python中，整数的位数并不是32位，所以自己限定一下
             if n & 1 == 1:
                 count += 1
             n = n >> 1
-            nums += 1
         return count
 
-import math
-math.isclose()
