ef6aee94c7990d734271c204034ec273b665226d
godzzbboss <1027785756@qq.com>
2019-10-07 12:32:41 +0800

    1

--- 剑指offer/数组中出现次数超过一半的数字.py
+++ 剑指offer/数组中出现次数超过一半的数字.py
@@ -14,7 +14,9 @@ class Solution:
         mid = len(array) // 2
         start = 0
         end = len(array) - 1
+        print(array)
         index = self.partition(array, start, end)  # 得到基元的下标
+        print(array)
         while index != mid:
             if index < mid: # index < mid, 只能在index右边的数中进行寻找
                 start = index + 1
@@ -28,18 +30,18 @@ class Solution:
     def partition(self, array, start, end):
         """找到基元在排序数组中的最终位置"""
         pivot = array[start]
-        p1 = start + 1
+        p1 = start
         p2 = end
 
-        while p1 <= p2:
-            while p1 <= p2 and array[p2] >= pivot:
+        while p1 < p2:
+            while p1 < p2 and array[p2] >= pivot:
                 p2 -= 1
-            while p1 <= p2 and array[p1] <= pivot:
+            while p1 < p2 and array[p1] <= pivot:
                 p1 += 1
             if p1 < p2:
                 array[p1], array[p2] = array[p2], array[p1]
-        array[start], array[p2] = array[p2], array[start] # 将基元放在最终的位置
-        return p2 # 返回基元的位置
+        array[start], array[p1] = array[p1], array[start] # 将基元放在最终的位置
+        return p1 # 返回基元的位置
 
     def check(self, array):
         """检查array是否合法"""
@@ -58,5 +60,5 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
-    array = [111,112]
+    array = [4, 2, 2]
     print(s.test(array))
